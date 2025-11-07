3531e58e3d1cff1efc8db93f26edeabcc55dbdeb
ndadri <adrianfalcones08@gmail.com>
2025-11-06 20:53:00 -0500

    Cabeceros ahora bien :)

--- src/main/java/service/ProductoServiceImplement.java
+++ src/main/java/service/ProductoServiceImplement.java
@@ -9,7 +9,8 @@ import java.util.List;
 public class ProductoServiceImplement implements ProductoService{
     @Override
     public List<Producto> listar() {
-        return Arrays.asList(new Producto(1L,"laptop", "computacion",523.21),
+        return Arrays.asList(
+                new Producto(1L,"laptop", "computacion",523.21),
                 new Producto(2L,"Mouse","inalambrico",15.25),
                 new Producto(3L,"Impresora", "tinta continua",256.25));
     }
